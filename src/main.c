d47dbd5b3d685f2d915351abba8a77474abe779c
faineance <faineance@users.noreply.github.com>
2016-12-31 14:45:17 +0000

    init bootable uefi app

--- src/main.c
+++ src/main.c
@@ -16,9 +16,9 @@ efi_main (EFI_HANDLE image, EFI_SYSTEM_TABLE *systab)
            "██╔═██╗ ██╔══╝  ██╔═██╗     ██║   ██║╚════██║\n"
            "██║  ██╗███████╗██║  ██╗    ╚██████╔╝███████║\n"
            "╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝     ╚═════╝ ╚══════╝\n");
+    uefi_call_wrapper(ST->ConOut->OutputString,2,ST->ConOut, ST->FirmwareVendor);
     Print(L"\n\n\nPress any key to exit\n");
     WaitForSingleEvent(ST->ConIn->WaitForKey, 0);
-
     return EFI_SUCCESS;
 }
 
